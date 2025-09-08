b8b7ce00e0937948525dcf9b16af0f9f9c9d6ab1
sdj3959 <dj9099@naver.com>
2025-09-08 12:24:55 +0900

    장바구니 모달에서 삭제 기능 구현

--- src/context/cart-context.js
+++ src/context/cart-context.js
@@ -8,7 +8,8 @@ const defaultValue = {
   openModal: () => {},
   closeModal: () => {},
   cartItems: [],
-  addToCartItem: (cartItems) => {}
+  addToCartItem: (cartItems) => {},
+  removeFromCartItem: (id) => {},
 };
 
 const CartContext = createContext(defaultValue);
