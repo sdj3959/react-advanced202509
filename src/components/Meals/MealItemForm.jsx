7fd44ff5e15285dcc430533961733799d35bde72
sdj3959 <dj9099@naver.com>
2025-09-08 09:28:12 +0900

    가변 props 전달

--- src/components/Meals/MealItemForm.jsx
+++ src/components/Meals/MealItemForm.jsx
@@ -1,4 +1,4 @@
-import Input from '../UI/Input.jsx';
+import Input from '../UI/Input';
 import styles from './MealItemForm.module.scss';
 
 const MealItemForm = (props) => {
@@ -6,14 +6,14 @@ const MealItemForm = (props) => {
     <form className={styles.form}>
       <Input
         label='수량'
-        // input={{
-        //   id: 'amount_' + props.id,
-        //   type: 'number',
-        //   min: '1',
-        //   max: '5',
-        //   step: '1',
-        //   defaultValue: '1',
-        // }}
+        inputAttr={{
+          id: 'amount_' + props.id,
+          type: 'number',
+          min: '1',
+          max: '5',
+          step: '1',
+          defaultValue: '1',
+        }}
       />
       <button>담기</button>
     </form>
